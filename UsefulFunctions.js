e3bd5c4c688c50a4c800765b45597d3e6df48194
UsefulFunctionsjs <kitty2bahoo2@gmail.com>
2017-04-29 10:58:36 -0400

    Update UsefulFunctions.js

--- UsefulFunctions.js
+++ UsefulFunctions.js
@@ -1,4 +1,3 @@
-
 function euclidean(x1,y1,x2,y2) {
 		
 			return(Math.sqrt(Math.pow((Math.abs(x1-x2)),2)+Math.pow((Math.abs(y1-y2)),2)))
@@ -12,4 +11,44 @@ function randInt(int1, int2) {
 }
 function randNum(num1, num2) {
 	return(Math.random()*(num2-num1)+num1)
+}
+function sum(mylist) {
+	var mysum = 0
+	for (var i = 0; i < mylist.length; i++) {
+		mysum += mylist[i]
+		}
+function mean(mylist) {
+	var av = sum(mylist)
+	
+	return(av/mylist.length)
+
+}
+function max(mylist) {
+	var t=mylist[0]
+	for (var i = 0; i < mylist.length; i++) {
+		if(mylist[i] > t){
+			t=mylist[i]
+		}
+	
+	}
+return(t)
+}
+function min(mylist) {
+	var t=mylist[0]
+	for (var i = 0; i < mylist.length; i++) {
+		if(mylist[i] < t){
+			t=mylist[i]
+		}
+	
+	}
+return(t)
+}
+function overThreshold(mylist, t){
+	over=0
+	for (var i = 0; i < mylist.length; i++) {
+		if (mylist[i] > t){
+			over ++
+		}
+	}
+	return(over)
 }
