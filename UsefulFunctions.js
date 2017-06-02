5fbacfe89ff07b04f32e6553516705b49d18a4fb
UsefulFunctionsjs <kitty2bahoo2@gmail.com>
2017-06-02 18:04:30 -0400

    Update UsefulFunctions.js

--- UsefulFunctions.js
+++ UsefulFunctions.js
@@ -1,4 +1,5 @@
 
+
 // Finds the distance between 2 points on 2D plane using their x and y positions.
 function euclidean(x1,y1,x2,y2) {
 		
@@ -15,52 +16,140 @@ function randInt(int1, int2) {
 function randNum(num1, num2) {
 	return(Math.random()*(num2-num1)+num1)
 }
-//Finds the sum of the numbers in "mylist".
-function sum(mylist) {
+//Finds the sum of the numbers in "myList".
+function sum(myList) {
  	var mysum = 0
-	for (var i = 0; i < mylist.length; i++) {
-		mysum += mylist[i]
+	for (var i = 0; i < myList.length; i++) {
+		mysum += myList[i]
 		}
 	return mysum
 }
-//Finds the mean, or average of the numbers in "mylist".
-function mean(mylist) {
-	var av = sum(mylist)
+//Finds the mean, or average of the numbers in "myList".
+function mean(myList) {
+	var av = sum(myList)
 	
-	return(av/mylist.length)
+	return(av/myList.length)
 
 }
-//Finds the highest number in "mylist".
-function max(mylist) {
-	var greatest=mylist[0]
-	for (var i = 0; i < mylist.length; i++) {
-		if(mylist[i] > greatest){
-			greatest=mylist[i]
+//Finds the highest number in "myList".
+function max(myList) {
+	var greatest=myList[0]
+	for (var i = 0; i < myList.length; i++) {
+		if(myList[i] > greatest){
+			greatest=myList[i]
 		}
 	
 	}
 return(greatest)
 }
-//Finds the lowest number in "mylist".
-function min(mylist) {
-	var least=mylist[0]
-	for (var i = 0; i < mylist.length; i++) {
-		if(mylist[i] < least){
-			least=mylist[i]
+//Finds the lowest number in "myList".
+function min(myList) {
+	var least=myList[0]
+	for (var i = 0; i < myList.length; i++) {
+		if(myList[i] < least){
+			least=myList[i]
 		}
 	
 	}
 return(least)
 }
-//Finds how many numbers in "mylist" that are over the threshold, "t".
-function overThreshold(mylist, t){
+//Finds how many numbers in "myList" that are over the threshold, "t".
+function overThreshold(myList, t){
 	var over=0
-	for (var i = 0; i < mylist.length; i++) {
-		if (mylist[i] > t){
+	for (var i = 0; i < myList.length; i++) {
+		if (myList[i] > t){
 			over ++
 		}
 	}
 	return(over)
 }
+
+
+//finds the index of the smallest number in "myList"
+//new
+function minIndex(myList) {
+	var least=0
+		for (var i = 0; i < myList.length; i++) {
+			if(myList[i] < myList[least]){
+				least=i
+			}
+		
+		}
+	
+	return(least)
+}
+//finds the index of the largest number in "myList"
+//new
+function maxIndex(myList) {
+	var most=0
+		for (var i = 0; i < myList.length; i++) {
+			if(myList[i] > myList[most]){
+				most=i
+			}
+		
+		}
+	
+	return(most)
+}
+//finds the median of "myList"
+//new
+function median(myList){
+	for(var i = 0; myList.length < 3; i++){
+		var smallIndex = minIndex(myList)
+		var largeIndex = maxIndex(myList)
+		if(smallIndex != -1) {
+			myList.splice(smallIndex, 1);
+		}
+		if(largeIndex != -1) {
+				myList.splice(largeIndex, 1);
+		}
+	}
+return mean(myList)
+}
+// Returns "myList" with "num1" and "num2" swapped
+//new
+function swap(myList, num1, num2) {
+	var foo = myList[num1]
+	var gaa = myList[num2]
+	myList[num1] = gaa
+	myList[num2] = foo
+	return myList
+}
+//sorts "myList" into acending order
+//advantage over bubble:faster
+//disadvantage over bubble:uses more data
+//new
+function sSort(myList){
+	var length = myList.length
+    var sortedArray = []
+
+	for (var i = 0; i < length; i ++) {
+		var least = min(myList)
+		sortedArray.push(least)
+		var index = findMinIndex(myList);
+	if(index != -1) {
+		myList.splice(index, 1);
+	}
+	}
+	
+	return sortedArray
+}
+//sorts"myList" into acending order
+//advantage over sSort: uses less data
+//disadvantage over sSort: slower
+//new
+function bubble(myList){
+	
+	for(var i = 0; i < myList.length; i++){
+		for(var j = 0; j < myList.length-1; j++){
+			if(myList[j]>myList[j+1]){
+				swap(myList, j, j+1)
+			}
+		}
+	}
+	return(myList)
+	
+}
+
 
 
